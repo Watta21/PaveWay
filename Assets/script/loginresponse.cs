91d55843eb585b4d92779be07ceb0db9d7e30f84
Watta21 <cool4cool456@gmail.com>
2026-03-09 13:58:00 +0800

    Delete Assets directory

--- Assets/script/loginresponse.cs
+++
@@ -1,9 +0,0 @@
-[System.Serializable]
-public class LoginResponse
-{
-    public bool success;
-    public string status;   // This MUST exist
-    public string username;
-    public string email;
-    public string message;  // optional error message
-}
