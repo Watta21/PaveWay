431c1622be637a6fa8361d4eef2d995cbb1f8c50
Watta21 <cool4cool456@gmail.com>
2026-03-09 13:31:19 +0800

    Add files via upload

---
+++ Assets/script/Content Placement/PlaceOnAPlane.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using Immersal.XR;
+using UnityEngine.Serialization;
+
+namespace Immersal.Samples.ContentPlacement
+{
+    public class PlaceOnAPlane : MonoBehaviour
+    {
+        [SerializeField]
+        private List<GameObject> m_objects = new List<GameObject>();
+        [FormerlySerializedAs("m_ARSpace")] [SerializeField]
+        private XRSpace m_XRSpace = null;
+
+        public void Place(int index)
+        {
+            Transform cam = Camera.main?.transform;
+            m_XRSpace = FindObjectOfType<XRSpace>();
+
+            if (cam != null && m_objects[index] != null && m_XRSpace != null)
+            {
+                RaycastHit hit;
+                Vector3 direction = cam.forward;
+                Vector3 origin = cam.position;
+
+                if (Physics.Raycast(origin, direction, out hit, Mathf.Infinity))
+                {
+                    if (hit.collider != null)
+                    {
+                        ARPlane arPlane = hit.collider.GetComponentInParent<ARPlane>();
+                        if (arPlane)
+                        {
+                            Vector3 pos = hit.point;
+                            GameObject go = Instantiate(m_objects[index], m_XRSpace.transform);
+                            go.transform.localRotation = Quaternion.identity;
+                            go.transform.position = pos;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
