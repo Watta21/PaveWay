5e23569e05a1206bcb176bfa3550e889e75a56ba
Watta21 <cool4cool456@gmail.com>
2026-03-09 13:43:31 +0800

    Add files via upload

---
+++ Assets/script/util/CryptoUtil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Immersal.Samples.Util
+{
+    public static class CryptoUtil
+    {
+        public static string MD5(byte[] bytes)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] hashBytes = md5.ComputeHash(bytes);
+
+            string hashString = "";
+
+            for (int i = 0; i < hashBytes.Length; i++)
+                hashString += Convert.ToString(hashBytes[i], 16).PadLeft(2, '0');
+
+            return hashString.PadLeft(32, '0');
+        }
+
+        public static string SHA256(byte[] bytes)
+        {
+            SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider();
+            byte[] hashBytes = sha256.ComputeHash(bytes);
+            string hashString = "";
+            for (int i = 0; i < hashBytes.Length; i++)
+                hashString += Convert.ToString(hashBytes[i], 16).PadLeft(2, '0');
+            return hashString.PadLeft(64, '0');
+        }
+    }
+}
